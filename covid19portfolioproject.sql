3a1590d6f1756b39cfa30556495187dcb81c80fb
rube1017 <93703103+rube1017@users.noreply.github.com>
2021-11-04 13:40:59 +0530

    Update covid19portfolioproject.sql

--- covid19portfolioproject.sql
+++ covid19portfolioproject.sql
@@ -1,7 +1,10 @@
 /*
 
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
 select * from coviddeaths
-   where continent is not null
+where continent is not null
     
 --select * from covidvaccination
 
